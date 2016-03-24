ac56f2299d4cfb529c6428113eac91471990042c
pirosikick <pirosikick@gmail.com>
2016-03-24 23:01:52 +0900

    Modified gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,17 @@
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
-const run = require('run-sequence');
-
-const ava = src => gulp.src(src).pipe($.ava());
-
-const src = {
-  js: ['src/**/*.js'],
-  test: ['test/**/*.js']
-};
 
 gulp.task('default', ['watch']);
-gulp.task('test', done => run('babel', 'ava', done));
+gulp.task('test', ['ava']);
 gulp.task('watch', ['babel'], () => {
-  gulp.watch(src.js, ['babel']);
-  gulp.watch(src.test, ['ava']);
+  gulp.watch(['lib/**/*.js', 'test/**/*.js'], ['test']);
 });
 gulp.task('babel', () => {
-  gulp.src(src.js)
+  gulp.src(['src/**/*.js'])
     .pipe($.plumber())
-    .pipe($.babel({ presets: ['es2015'] }))
+    .pipe($.babel())
     .pipe(gulp.dest('lib'));
 });
-gulp.task('ava', () =>
-  gulp.src(src.test).pipe($.ava())
+gulp.task('ava', ['babel'], () =>
+  gulp.src(['test/**/*.js']).pipe($.ava())
 );
