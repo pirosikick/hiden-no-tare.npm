fce7fee501f5d8a9911f1540d4fbb340355864cc
Hiroyuki Anai <pirosikick@gmail.com>
2017-02-18 00:02:11 +0900

    Add copy.js

---
+++ copy.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+const path = require('path');
+const fs = require('fs');
+const VError = require('verror');
+
+const targetFiles = [
+  '.babelrc',
+  '.eslintrc.json',
+  '.flowconfig',
+  '.gitignore',
+  '.travis.yml',
+  'package.json',
+  'yarn.lock',
+];
+
+function copy(src, dist) {
+  return new Promise((resolve, reject) => {
+    const r = fs.createReadStream(src);
+    const w = fs.createWriteStream(dist);
+
+    r.on('error',
+      err => reject(new VError(err, "failed to read from '%s'", src)));
+    w.on('error',
+      err => reject(new VError(err, "failed to write to '%s'", dist)));
+    w.on('close', () => resolve());
+
+    r.pipe(w);
+  });
+}
+
+const copyPromise = targetFiles.reduce((promise, file) => (
+  promise.then(() => {
+    console.log(`copy ${file}...`);
+    const src = path.join(__dirname, file);
+    const dist = path.join(process.cwd(), file);
+    return copy(src, dist)
+  })
+), Promise.resolve());
+
+copyPromise.catch(err => {
+  console.log('Failed to copy files:', err.message);
+});
